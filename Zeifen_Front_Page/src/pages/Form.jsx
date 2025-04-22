1c41046945f449f9184f8b4a9114e6785e16b3ca
Zeifen <60271815+Zeifen@users.noreply.github.com>
2025-04-21 20:33:16 -0600

    Se agrega useState y onChange para inputs

--- Zeifen_Front_Page/src/pages/Form.jsx
+++ Zeifen_Front_Page/src/pages/Form.jsx
@@ -1,15 +1,30 @@
+import {useState} from 'react';
 
 const Form = () => {
+
+  const [ name, setName ] = useState('');
+  const [ lastName, setLastName ] = useState('');
+  const [ email, setEmail ] = useState('');
+  const [ company, setCompany ] = useState('');
+  const [ direction, setCompanyDirection ] = useState('');
+  const [ country, setCountry ] = useState('');
+  const [ state, setState ] = useState('');
+  const [ description, setDescription ] = useState('');
+
+  const handleValue = () => {
+
+  };
+
   return (
     <>
     <div>
       <div>
         <h4 className="mb-3">Ingresa tu información</h4>
-        <form className="needs-validation" noValidate>
+        <form className="needs-validation" >
           <div className="row g-3">
             <div className="col-sm-6">
               <label htmlFor="firstName" className="form-label">Nombre</label>
-              <input type="text" className="form-control" id="firstName" placeholder="" value="" required/>
+              <input type="text" class="form-control" id="firstName" placeholder="" value={name} onChange={(e) => {setName(e.target.value)}} required/>
               <div className="invalid-feedback">
                 Valid first name is required.
               </div>
@@ -17,7 +32,7 @@ const Form = () => {
 
             <div className="col-sm-6">
               <label htmlFor="lastName" className="form-label">Apellidos</label>
-              <input type="text" className="form-control" id="lastName" placeholder="" value="" required/>
+              <input type="text" className="form-control" id="lastName" placeholder="" value={lastName} onChange={(e) => {setLastName(e.target.value)}} required/>
               <div className="invalid-feedback">
                 Valid last name is required.
               </div>
@@ -25,7 +40,7 @@ const Form = () => {
 
             <div className="col-12">
               <label htmlFor="email" className="form-label">Email <span className="text-body-secondary">(Optional)</span></label>
-              <input type="email" className="form-control" id="email" placeholder="usuario@ejemplo.com"/>
+              <input type="email" className="form-control" id="email" placeholder="usuario@ejemplo.com"value={email} onChange={(e) => {setEmail(e.target.value)}}/>
               <div className="invalid-feedback">
                 Please enter a valid email address for shipping updates.
               </div>
@@ -33,12 +48,12 @@ const Form = () => {
 
             <div className="col-12">
               <label htmlFor="address2" className="form-label">Empresa<span className="text-body-secondary"></span></label>
-              <input type="text" className="form-control" id="address2" placeholder="Nombre de la empresa"/>
+              <input type="text" className="form-control" id="address2" placeholder="Nombre de la empresa" value={company} onChange={(e) => {setCompany(e.target.value)}}/>
             </div>
 
             <div className="col-12">
               <label htmlFor="address" className="form-label">Dirección de empresa</label>
-              <input type="text" className="form-control" id="address" placeholder="" required/>
+              <input type="text" className="form-control" id="address" placeholder="" value={direction} onChange={(e) => {setCompanyDirection(e.target.value)}} required/>
               <div className="invalid-feedback">
                 Please enter your shipping address.
               </div>
@@ -46,7 +61,7 @@ const Form = () => {
 
             <div className="col-md-6">
               <label htmlFor="country" className="form-label">País</label>
-              <select className="form-select" id="country" required>
+              <select className="form-select" id="country" value={country} onChange={(e) => {setCountry(e.target.value)}} required>
                 <option value="">Selecciona</option>
                 <option>United States</option>
               </select>
@@ -57,7 +72,7 @@ const Form = () => {
 
             <div className="col-md-6">
               <label htmlFor="state" className="form-label">Estado</label>
-              <select className="form-select" id="state" required>
+              <select className="form-select" id="state" value={state} onChange={(e) => {setState(e.target.value)}} required>
                 <option value="">Selecciona</option>
                 <option>California</option>
               </select>
@@ -68,7 +83,7 @@ const Form = () => {
 
             <div className='form-group mb-2'>
               <label htmlFor="state" className="form-label">Descripción</label>
-              <textarea type="tnumber" id='descripcion' className="form-control" name='descripcion' placeholder='Agrega el motivo'></textarea>
+              <textarea type="tnumber" id='descripcion' className="form-control" name='descripcion' placeholder='Agrega el motivo' value={description} onChange={(e) => {setDescription(e.target.value)}}></textarea>
             </div>
           </div>
 
@@ -76,7 +91,7 @@ const Form = () => {
 
           <div className="row gy-3">
             <div className='col-md-12 general-text'>
-            <button className="btn btn-primary btn-lg form_button" type="submit">Enviar</button>
+            <button className="btn btn-primary btn-lg form_button" type="submit" onClick={handleValue}>Enviar</button>
           </div>  
         </div>
           <hr className="my-4"/> 
