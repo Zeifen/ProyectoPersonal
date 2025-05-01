722aac17a5b5ca89f2d865e93a5dfda051dd9eb7
Zeifen <60271815+Zeifen@users.noreply.github.com>
2025-04-30 19:16:21 -0600

    Feat: Se crea Carrusel

---
+++ Zeifen_Front_Page/src/data/data.jsx
@@ -0,0 +1,24 @@
+const images = [{
+    id: 1,
+    title: "Cer1",
+    source: './../cer1.jpg',
+    caption: 'Scrum Developer',
+    description: 'Certificado de Scrum Developer',
+},
+{
+    id: 2,
+    title: "Cer2",
+    source: '../../cer2.jpg',
+    caption: 'Desarrollo Seguro 2.0',
+    description: 'Certificado de Desarrollo Seguro 2.0 - Net4Skills',
+},
+{
+    id: 3,
+    title: "Cer3",
+    source: '../../cer3.jpg',
+    caption: 'SQL y MySQL',
+    description: 'Diploma de SQL y MySQL',
+},
+];
+
+export { images };
