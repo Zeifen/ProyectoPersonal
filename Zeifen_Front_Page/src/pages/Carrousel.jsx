e584586c1388b692d67f6750f86eaf35b88740a6
Zeifen <60271815+Zeifen@users.noreply.github.com>
2025-05-20 16:42:48 -0600

    Feat: se realiza carrusel

--- Zeifen_Front_Page/src/pages/Carrousel.jsx
+++ Zeifen_Front_Page/src/pages/Carrousel.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import Carousel from "react-bootstrap/Carousel";
+import { images } from "../data/data.jsx";
 
 const Carrousel = () => {
   return (
     <>
-      <div className='general-text'>Carrousel</div>
+      <div className="container carousel-size">
+        <Carousel>
+          {images.map((image) => (
+            <Carousel.Item interval={2000} key={image.id}>
+              <img
+                className="carousel-image"
+                src={image.source}
+                alt={image.title}
+              />
+            </Carousel.Item>
+          ))}
+        </Carousel>
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default Carrousel;
